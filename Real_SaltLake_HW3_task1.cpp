78988625c4383d4402132d41ec12b0fc0ececa2a
Daryl <daryladams@mail.weber.edu>
2017-09-28 15:09:32 -0600

    Added showDate function to project, and is fully working working

--- Real_SaltLake_HW3_task1.cpp
+++ Real_SaltLake_HW3_task1.cpp
@@ -9,20 +9,48 @@ private:
     int year;
 
 public:
-    void getDate(int a, int b, int c) {
+
+    // Ask for user to enter in a date
+    void setDate(int a, int b, int c)
+    {
 
         month = a;
         day = b;
         year = c;
+    }
+    void getDate()
+    {
+        int a,b,c;
+        char tempChar ;
 
-        char tempChar = '/';
-
-        cout << "Enter a date (format 12/31/99: " << endl;
         cin >> a >> tempChar >> b >> tempChar >> c;
+        setDate(a,b,c);
     }
 
+    //Display users input
+    void showDate()
+    {
+
+        cout << month << "/" << day << "/" << year<<endl;
+
+
+    }
 };
-int main()
+int main(void)
 {
+    Date d1, d2;
+    cout << " Enter a date (format 12/31/99: " << endl;
+    d1.getDate();
+    cout << "Enter another date: ";
+    d2.getDate();
+
+    cout << "date 1 = ";
+    d1.showDate();
+    cout << "date 2 = ";
+    d2.showDate();
+
+
+
+
     return 0;
 }
