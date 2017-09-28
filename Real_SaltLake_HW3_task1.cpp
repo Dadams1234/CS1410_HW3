28450706330e45ba29a57d3377aa9a77b0bee5e9
Daryl <daryladams@mail.weber.edu>
2017-09-28 10:38:45 -0600

    Intial Commit with getDate function

--- Real_SaltLake_HW3_task1.cpp
+++ Real_SaltLake_HW3_task1.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+class Date
+{
+private:
+    int month;
+    int day;
+    int year;
+
+public:
+    void getDate(int a, int b, int c) {
+
+        month = a;
+        day = b;
+        year = c;
+
+        char tempChar = '/';
+
+        cout << "Enter a date (format 12/31/99: " << endl;
+        cin >> a >> tempChar >> b >> tempChar >> c;
+    }
+
+};
+int main()
+{
     return 0;
 }
